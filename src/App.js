32975d97f8b663996ada1b67259453ceabd3bc6d
Frantz <fpaul787@gmail.com>
2020-05-31 23:06:25 -0400

    added link to navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import { Navbar } from "react-bootstrap"
+import {Nav, Navbar, NavItem} from 'react-bootstrap'
 import Routes from './Routes'
 import './App.css';
 
 function App() {
   return (
     <div className="App container">
-      <Navbar fiuld collapseOnSelect>
+      <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
             <Link to="/">9/10</Link>
           </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem href="/signup">Signup</NavItem>
+            <NavItem href="/login">Login</NavItem>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes/>
     </div>
   );
 }
+
 
 export default App;
